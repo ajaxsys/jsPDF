efafd0e47f0bfb8f50585b84077bc9e94514de67
fangdehui <javaeecoding@gmail.com>
2021-12-07 16:46:51 +0900

    add a example

--- examples/js/images.js
+++ examples/js/images.js
@@ -1,50 +1,137 @@
+var config = {
+    default: {
+        name: 'l', 
+        unit: 'mm', 
+        size: [297, 210],
+        font: {
+            name: 'Mplus2-Regular', 
+            variant: 'normal',
+            size: 16,
+            color: [0,0,255]
+        },
+    },
+    layers: [
+        {
+            position: [0, 0],
+            image: "https://i.imgur.com/R09pXIX.png",
+            type: "png",
+        },
+        {
+            position: [70, 48],
+            text: "たなか　たろう",
+            font: {
+                url: "test/reference/MouhitsuBold.ttf",
+                name: 'Mouhitsu', 
+                variant: 'bold',
+                color: [255, 0, 0],
+                size: 16
+            },
+        },
+        {
+            position: [48, 90],
+            text: "商品１",
+        },
+        {
+            position: [48, 103],
+            text: "商品2",
+        },
+        {
+            position: [48, 116],
+            text: "商品3",
+        },
+        {
+            position: [70, 148],
+            text: "請求者　太郎",
+            font: {
+                name: 'Roboto-Regular', 
+                variant: 'normal',
+                color: [0, 255, 0],
+                size: 20
+            },
+        },
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+// 下記はUtilとして利用可能
 // A4 横
-var doc = new jsPDF('l', 'mm', [297, 210]);
-// A4 縦
-// new jsPDF('p', 'mm', [297, 210]);
-console.log(doc.getFontList());
+var doc = new jsPDF(
+    config.default.name,
+    config.default.unit,
+    config.default.size
+);
 
-doc.addImage("https://i.imgur.com/R09pXIX.png", "PNG", 0, 0);
+render(doc, config);
 
-doc.addFont("test/reference/MouhitsuBold.ttf", "Mouhitsu", "bold");
-doc.setFont("Mouhitsu", "bold"); // 文字のフォント
+function render(doc, config) {
 
-doc.setFontSize(20); // 文字サイズ
-doc.setTextColor(255,0,0); // 文字色
+    // A4 縦
+    // new jsPDF('p', 'mm', [297, 210]);
+    console.log("Supported fonts:", doc.getFontList());
 
-doc.text("たなか　たろう", 70, 48); // 文字と表示の座標
+    config.layers.forEach(el => {
+        debugger
+        if (el.text) {
+            el.font = Object.assign(
+                {}, 
+                config.default.font,
+                el.font || {}, 
+            );
+            renderText(doc, el)
+        } else if (el.image) {
+            renderImage(doc, el)
+        } else {
+            console.error("Not support yet", el);
+        }
+    });
+}
 
-doc.setFont("Roboto-Regular", "normal"); // 文字のフォント
-doc.setFontSize(25);
-doc.setTextColor(0,0,255); // 文字色
-doc.text("日本語テスト(Roboto-Regular normal)", 0, 25);
+function renderText(doc, el) {
+    if (el.font.url) {
+        doc.addFont(
+            el.font.url,
+            el.font.name, 
+            el.font.variant);
+    }
+    doc.setFont(
+        el.font.name, 
+        el.font.variant);
+    doc.setFontSize(el.font.size);
+    if (el.font.color) {
+        doc.setTextColor(
+            el.font.color[0],
+            el.font.color[1],
+            el.font.color[2],
+        );
+    }
+    doc.text(
+        el.text, 
+        el.position[0], 
+        el.position[1]
+    );
+}
+function renderImage(doc, el) {
+    doc.addImage(
+        el.image, 
+        el.type, 
+        el.position[0], 
+        el.position[1]);
 
-// 'Mplus2-Regular', 'normal'
-doc.setFont('Mplus2-Regular', 'normal'); // 文字のフォント
-doc.setFontSize(25);
-doc.setTextColor(0,0,255); // 文字色
-doc.text("フォントはMplus2-Regular使う", 0, 125);
+}
 
 
-
-
-// https://jsfiddle.net/Domt301/bu9db10d/
-// <div id="example1"></div>
-// <script src="/js/pdfobject.min.js"></script>
-// <script src="/js/jspdf.min.js"></script>
-
-// <script>
-//   PDFObject.embed(createPDF(), "#example1");
-            
-//   function createPDF() {
-//     var doc = new jsPDF();
-
-//     doc.setFont("helvetica");
-//     doc.setFontStyle("bold");
-                
-//     doc.setFontSize(20);
-//     doc.text('TEST PDF...', 105, 20, 'center');
-
-//     return doc.output('datauristring');    
-//   }
-// </script>
+// 注：
+// font追加方法
+// 1) fontのttfファイルを下記のURLでjsをbase64に変換する。例：
+//    https://rawgit.com/MrRio/jsPDF/master/fontconverter/fontconverter.html
+// 2) 上記のjsをhtmlのscriptタグとしてimportする。例：
+// 	<script type="module"  src="./test/reference/fonts/Roboto-Regular-normal.js"></script>
