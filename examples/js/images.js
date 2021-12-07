81db753d10b0431ce0241dcee14e5c7ff75e7cb3
fangdehui <javaeecoding@gmail.com>
2021-12-07 15:38:52 +0900

    add try jp

--- examples/js/images.js
+++ examples/js/images.js
@@ -1,7 +1,50 @@
-// Don't forget, that there are CORS-Restrictions. So if you want to run it without a Server in your Browser you need to transform the image to a dataURL
-// Use http://dataurl.net/#dataurlmaker
-var doc = new jsPDF();
+// A4 横
+const doc = new jsPDF('l', 'mm', [297, 210]);
+// A4 縦
+// new jsPDF('p', 'mm', [297, 210]);
+// console.log(doc.getFontList());
 
-doc.setFontSize(40);
-doc.text("Octonyan loves jsPDF", 35, 25);
-doc.addImage("examples/images/Octonyan.jpg", "JPEG", 15, 40, 180, 180);
+doc.addImage("https://i.imgur.com/R09pXIX.png", "PNG", 0, 0);
+
+doc.addFont("test/reference/MouhitsuBold.ttf", "Mouhitsu", "bold");
+doc.setFont("Mouhitsu", "bold"); // 文字のフォント
+
+doc.setFontSize(20); // 文字サイズ
+doc.setTextColor(255,0,0); // 文字色
+
+doc.text("たなか　たろう", 70, 48); // 文字と表示の座標
+
+// doc.setFont("Roboto-Regular", "normal"); // 文字のフォント
+// doc.setFontSize(25);
+// doc.setTextColor(0,0,255); // 文字色
+// doc.text("PDF日本", 0, 25);
+
+// // 'Mplus2-Regular', 'normal'
+// doc.setFont('Mplus2-Regular', 'normal'); // 文字のフォント
+// doc.setFontSize(25);
+// doc.setTextColor(0,0,255); // 文字色
+// doc.text("PDF日本2", 0, 25);
+
+
+
+
+// https://jsfiddle.net/Domt301/bu9db10d/
+// <div id="example1"></div>
+// <script src="/js/pdfobject.min.js"></script>
+// <script src="/js/jspdf.min.js"></script>
+
+// <script>
+//   PDFObject.embed(createPDF(), "#example1");
+            
+//   function createPDF() {
+//     var doc = new jsPDF();
+
+//     doc.setFont("helvetica");
+//     doc.setFontStyle("bold");
+                
+//     doc.setFontSize(20);
+//     doc.text('TEST PDF...', 105, 20, 'center');
+
+//     return doc.output('datauristring');    
+//   }
+// </script>
